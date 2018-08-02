a8ad7e69b0daea5bef02cfb4afb62d299085b426
Mark Williams <markrwilliams94@gmail.com>
2018-08-02 14:29:20 -0400

    Actually updating after adding and ignoring virtual environment, oops

---
+++ DjangoWebProject1/env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Users\Mark Williams\source\repos\LearningDjangoWithMicrosoft\DjangoWebProject1\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
